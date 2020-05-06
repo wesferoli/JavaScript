a7d8759c9ca28a9b22d2e8d5560225b1a0a6853b
WesleyFerreir4 <wesleyfer.oli97@gmail.com>
2020-05-06 13:16:04 -0300

    estudos 06-05

---
+++ JavaScript/projetos/11_js_no_navegador/03_mover_no_dom/js/scripts.js
@@ -0,0 +1,9 @@
+console.log(document.body.childNodes); //childNode do body
+
+console.log(document.body.childNodes[1]); //div (childNodes[1])
+
+console.log(document.body.childNodes[1].childNodes); //childNode da div
+
+console.log(document.body.childNodes[1].childNodes[1]); //h1 da div
+
+console.log(document.body.childNodes[1].childNodes[1].innerText); //texto do h1
---
+++ JavaScript/projetos/11_js_no_navegador/06_by_class/js/scripts.js
@@ -0,0 +1,7 @@
+console.log(document.getElementsByTagName('li'));
+
+console.log(document.getElementsByClassName('itensAzuis'));
+
+console.log(document.getElementsByClassName('itensVermelhos'));
+
+console.log(document.getElementsByClassName('itens'));
