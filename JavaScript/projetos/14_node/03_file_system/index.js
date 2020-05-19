b0592840bb27ae9dbe9aa232402b8e01a13ba230
WesleyFerreir4 <wesleyfer.oli97@gmail.com>
2020-05-18 23:19:09 -0300

    estudos 18-05

---
+++ JavaScript/projetos/14_node/03_file_system/index.js
@@ -0,0 +1,17 @@
+let {readFile, writeFile} = require('fs');
+
+readFile("arquivo.txt", "utf8", (error, texto) => {
+    if(error) {
+        throw error;
+    } else {
+        console.log(texto);
+    }
+});
+
+writeFile("arquivo.txt", " - Harry Styles em 'Sign of the Times'", (error) => {
+    if(error) {
+        throw error;
+    } else {
+        console.log("Escreveu com sucesso!");
+    }
+});
